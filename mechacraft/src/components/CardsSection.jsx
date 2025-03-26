b5b66c43e871e7a293ee051ad15343a1eb714b96
MegaBOT07 <72640898+MegaBOT07@users.noreply.github.com>
2025-03-26 13:40:32 +0000

    Changes to be committed: 	modified:   mechacraft/src/components/CardsSection.jsx

--- mechacraft/src/components/CardsSection.jsx
+++ mechacraft/src/components/CardsSection.jsx
@@ -32,7 +32,7 @@ const CardsSection = memo(() => {
       text: "Revolutionize tasks with cutting-edge robotics solutions.",
     },
     {
-      image: "https://images.pexels.com/photos/2518861/pexels-photo-2518861.jpeg?auto=compress&cs=tinysrgb&w=800",
+      image: "https://images.pexels.com/photos/974316/pexels-photo-974316.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       alt: "Agriculture",
       text: "Improves efficiency and productivity in farming practices.",
     },
