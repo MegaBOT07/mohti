24876248dff472c2288cd765c939ed15d22ddce6
MegaBOT07 <72640898+MegaBOT07@users.noreply.github.com>
2025-04-19 18:17:40 +0000

    Changes to be committed: 	modified:   mechacraft/src/components/Services.jsx 	modified:   mechacraft/src/components/TestPart.css

--- mechacraft/src/components/Services.jsx
+++ mechacraft/src/components/Services.jsx
@@ -4,134 +4,61 @@ import { Link } from "react-router-dom";
 import "./Services.css";
 const iotAgricultureImage = "https://media.istockphoto.com/id/1155183548/photo/lawn-mower-and-jerry-can-with-gasoline-on-the-background-of-green-grass.jpg?b=1&s=612x612&w=0&k=20&c=5NZnGGf7x0N7xXSn8T_7FeWWZmsKfUh0ekFKDxQu-Fo="; // Import your images
 const customSolutionsImage = "https://images.pexels.com/photos/442150/pexels-photo-442150.jpeg?auto=compress&cs=tinysrgb&w=800";
-const iotIntegrationImage = "https://images.pexels.com/photos/20226911/pexels-photo-20226911/free-photo-of-smart-home-devices-smartphone-blank-screen.jpeg?auto=compress&cs=tinysrgb&w=800";
-const iotMaintenanceImage = "https://images.pexels.com/photos/9242887/pexels-photo-9242887.jpeg";
 const iotConsultingImage = "https://images.pexels.com/photos/9242836/pexels-photo-9242836.jpeg?auto=compress&cs=tinysrgb&w=800";
-const healthcareIoTImage = "https://images.pexels.com/photos/7580249/pexels-photo-7580249.jpeg?auto=compress&cs=tinysrgb&w=800";
-const fleetTrackingImage = "https://images.pexels.com/photos/2800121/pexels-photo-2800121.jpeg?auto=compress&cs=tinysrgb&w=800";
 const roboticsLabsImage = "https://images.pexels.com/photos/3862632/pexels-photo-3862632.jpeg?auto=compress&cs=tinysrgb&w=800";
-const commercialDataImage = "https://media.istockphoto.com/id/1809660081/photo/modern-data-technology-center-server-racks-working-in-dark-facility-concept-of-internet-of.jpg?b=1&s=612x612&w=0&k=20&c=u1_CcoZe8CrbzWizkB0qxctCzlJSFkUDuQdHobSuv-s=";
-const industry40Image = "https://images.pexels.com/photos/9577249/pexels-photo-9577249.jpeg?auto=compress&cs=tinysrgb&w=800";
 const arVrImage = "https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&w=800";
 const droneTechImage = "https://images.pexels.com/photos/18660495/pexels-photo-18660495/free-photo-of-view-of-dji-air-3-drone-flying-in-the-forest-in-south-of-france.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
 const homeAutomationImage = "https://media.istockphoto.com/id/1014664366/photo/smart-home-automation-remote-control-internet-technology.jpg?b=1&s=612x612&w=0&k=20&c=kb_aNkbmD9qqL43IjaIkGACHkLtYqqkMAEuo-Uc0uxU=";
-const automotiveElectronicsImage = "https://images.pexels.com/photos/3862610/pexels-photo-3862610.jpeg?auto=compress&cs=tinysrgb&w=800";
-const personalTerminalsImage = "https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=800";
-
 const Services = () => {
   const services = [
     {
-      title: "IoT Solutions for Agriculture",
+      title: "Driving Agricultural and Healthcare Advancements with IoT",
       description:
-        "Revolutionize your farming practices with our IoT solutions for agriculture. Real-time monitoring of soil, weather, and crop health using IoT sensors. Automated irrigation and fertilization systems. AI-based data analytics for predictive farming. Integration with drones for crop spraying and monitoring.",
+        "Revolutionizing agriculture and healthcare through Internet of Things (IoT) solutions that enable smarter operations and better outcomes.",
       image: iotAgricultureImage,
       alt: "IoT Solutions for Agriculture",
     },
     {
-      title: "Drone Technology",
+      title: "Custom Web & App Development: Expert Consultancy and Tailored Solutions",
       description:
-        "Leverage the power of drone technology with our IoT-enabled solutions. IoT-enabled drones for surveillance, delivery, and inspection. AI-based flight path optimization. Real-time video and data streaming. Autonomous navigation capabilities.",
+        "Delivering secure, scalable, and user-friendly web and mobile applications, with expert consultancy guiding you through every stage—from concept and design to deployment and continuous optimization.",
+      image: customSolutionsImage,
+      alt: "Custom Web & App Development",
+    },
+    {
+      title: "Drone Technology: Aerial Solutions for Modern Industries",
+      description:
+        "Providing precise aerial data collection, mapping, and analysis to optimize operations across industries like agriculture, construction, and inspection.",
       image: droneTechImage,
       alt: "Drone Technology",
     },
     {
-      title: "IoT Integration",
+      title: "PCB Circuit Design Lab: Precision and Innovation in Electronics",
       description:
-        "Seamlessly integrate IoT devices and systems into your existing infrastructure. Our integration services ensure smooth connectivity and data flow, enabling you to leverage the full power of IoT without disrupting your operations.",
-      image: iotIntegrationImage,
-      alt: "IoT Integration",
-    },
-    {
-      title: "Healthcare IoT Solutions",
-      description:
-        "Transform healthcare delivery with our advanced IoT solutions. Patient monitoring systems with real-time data collection. Remote diagnostics and telemedicine integration. Smart wearables for health tracking. Secure medical data storage and analysis on the cloud.",
-      image: healthcareIoTImage,
-      alt: "Healthcare IoT Solutions",
-    },
-    {
-      title: "Fleet Tracking Solutions",
-      description:
-        "Optimize your fleet operations with our GPS-enabled tracking solutions. Real-time alerts for vehicle status and route optimization. Integration with fuel consumption and maintenance logs. Driver behavior analysis.",
-      image: fleetTrackingImage,
-      alt: "Fleet Tracking Solutions",
-    },
-    {
-
-      title: "IoT Maintenance",
-      description:
-        "Ensure the continuous performance and reliability of your IoT deployments with our comprehensive maintenance services. We provide proactive monitoring, troubleshooting, and support to keep your systems running smoothly.",
-      image: iotMaintenanceImage,
-      alt: "IoT Maintenance",
-    },
-    {
-      title: "Industry 4.0 Solutions",
-      description:
-        "Embrace the future of manufacturing with our Industry 4.0 solutions. Predictive maintenance systems for industrial equipment. IoT-enabled monitoring and control of production lines. Integration of robotics and automated guided vehicles (AGVs). Real-time data analytics and reporting dashboards.",
-      image: industry40Image,
-      alt: "Industry 4.0 Solutions",
-
-    },
-    {
-      title: "Home Automation",
-      description:
-        "Experience the convenience and comfort of a smart home with our automation solutions. Smart lighting, HVAC, and security systems. Voice-controlled and mobile app integration. Energy monitoring and optimization. AI-based personalized automation.",
-      image: homeAutomationImage,
-      alt: "Home Automation",
-
-    },
-    {
-      title: "IoT Consulting",
-      description:
-        "Unlock the potential of IoT with our expert consulting services. We provide strategic guidance to help you implement IoT solutions that align with your business goals. From initial assessment to deployment and beyond, we'll guide you through every step.",
+        "Designing high-quality, reliable, and scalable PCBs for various electronic products, from prototypes to large-scale production.",
       image: iotConsultingImage,
-      alt: "IoT Consulting",
+      alt: "PCB Circuit Design Lab",
     },
     {
-      title: "Robotics and IoT Research Labs",
+      title: "AI & Machine Learning: Smart Solutions for the Future",
       description:
-        "Foster innovation and learning with our robotics and IoT research labs. Hands-on kits for robotics and IoT experiments. Simulators for robotics and IoT systems. Cloud-based LMS and CMS integration. Collaborative tools for research and project management.",
-      image: roboticsLabsImage,
-      alt: "Robotics and IoT Research Labs",
-
-    },
-    {
-      title: "Commercial Equipment and Data Centers",
-      description:
-        "Ensure the reliability and efficiency of your commercial equipment and data centers with our IoT solutions. IoT-enabled commercial appliances. Secure data storage and retrieval systems. High-availability and fault-tolerant architecture. Scalable cloud-based solutions.",
-      image: commercialDataImage,
-      alt: "Commercial Equipment and Data Centers",
-
-    },
-    {
-      title: "AR and VR Solutions",
-      description:
-        "Enhance training and operations with our AR and VR solutions. Immersive training modules for industries and education. AR-based equipment maintenance guides. VR simulations for healthcare and automotive applications. Integration with IoT for real-time data overlay.",
+        "Leveraging AI and Machine Learning to create intelligent systems that automate processes, enhance decision-making, and drive innovation across industries like healthcare, finance, and manufacturing.",
       image: arVrImage,
-      alt: "AR and VR Solutions",
-
+      alt: "AI & Machine Learning",
     },
     {
-      title: "Custom IoT Solutions",
+      title: "Robotics: Automation for Enhanced Efficiency",
       description:
-        "We specialize in developing tailored IoT solutions designed to meet your unique requirements. Our team of experienced engineers and developers will create innovative solutions that address your specific challenges and drive your business forward.",
-      image: customSolutionsImage,
-      alt: "Custom IoT Solutions",
+        "Designing and developing advanced robotics solutions to automate complex tasks, improving efficiency, accuracy, and safety in industries such as manufacturing, logistics, and healthcare.",
+      image: roboticsLabsImage,
+      alt: "Robotics",
     },
     {
-      title: "Automotive Electronics",
+      title: "Technology Consulting and Integration",
       description:
-        "Advance your automotive solutions with our IoT-enabled electronics. IoT-enabled vehicle health monitoring systems. Integration with smart fleet management. Advanced driver-assistance systems (ADAS). Infotainment systems.",
-      image: automotiveElectronicsImage,
-      alt: "Automotive Electronics",
-
-    },
-    {
-      title: "Personal Terminals and Smart Equipment",
-      description:
-        "Enhance productivity and health with our personal terminals and smart equipment. Wearable devices for productivity and health monitoring. Integration with cloud services for data synchronization. AI-based recommendations and alerts. IoT-enabled smart equipment for various domains.",
-      image: personalTerminalsImage,
-      alt: "Personal Terminals and Smart Equipment",
-
+        "Helping businesses seamlessly integrate cutting-edge technologies to boost efficiency, reduce costs, and foster innovation.",
+      image: homeAutomationImage,
+      alt: "Technology Consulting and Integration",
     },
   ];
 
