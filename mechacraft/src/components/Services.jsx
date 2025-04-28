a79194f9edaf1ee75f890a71d0e479c64cfb22fb
MegaBOT07 <72640898+MegaBOT07@users.noreply.github.com>
2025-04-28 18:03:57 +0000

    Changes to be committed: 	modified:   mechacraft/src/components/Services.jsx

--- mechacraft/src/components/Services.jsx
+++ mechacraft/src/components/Services.jsx
@@ -1,7 +1,6 @@
 // Services.js
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import emailjs from '@emailjs/browser';
 import "./Services.css";
 
 const iotAgricultureImage = "./assets/service1.jpeg";
